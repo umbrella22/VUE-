98ae52c7b21ffeea034feb54f5c4d6ad0eb9e207
umbrella22 <syl1998@hotmail.com>
2018-12-21 17:59:05 +0800

    添加图标，之后着手封装

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,16 @@
     
     <!-- <addout></addout>
     <yu></yu> -->
-    <list></list>
+    <!-- 列表效果 -->
+    <!-- <list></list> -->
+    <!-- 动画效果练习 -->
+    <dh/>
     <!-- 这里的view指定新页面的位置 -->
     <router-view></router-view>
     <!-- 这里指的是新页面的入口 -->
     <!-- <router-link :to="{path:'404'}">错误页面</router-link> -->
   </div>
-  <!-- -->
+  <!-- vue的基础 -->
   <!-- <HelloWorld/> -->
   
 </template>
@@ -20,13 +23,15 @@
 import addout from './components/addout.vue'
 import yu from './components/text.vue'
 import list from './components/list.vue'
+import dh from './components/echaet.vue'
 export default {
   name: 'app',
   components: {
     // HelloWorld,
     addout,
     yu,
-    list
+    list,
+    dh
   },
   computed:{
     totalPrice(){
---
+++ src/components/echaet.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <div ref="myChart" class="demo"></div>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({}),
+  mounted() {
+    this.make();
+  },
+  methods: {
+    make() {
+      let myChart = this.$echarts.init(this.$refs.myChart);
+      myChart.setOption({
+        //   图形背景色
+        backgroundColor: "#2c343c",
+        // 标题
+        title: {
+          text: "饼状图",
+          left: "center",
+          top: 20,
+          textStyle: {
+            color: "#ccc"
+          }
+        },
+        // 用于鼠标指针放上去显示数值
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+
+        // visualMap: {
+        //   show: false,
+        //   min: 80,
+        //   max: 600,
+        //   inRange: {
+        //     colorLightness: [0, 1]
+        //   }
+        // },
+        series: [
+          {
+            name: "访问来源",
+            type: "pie",
+            // 中心图大小
+            radius: "55%",
+            center: ["50%", "50%"],
+            data: [
+              { value: 335, name: "直接访问" },
+              { value: 310, name: "邮件营销" },
+              { value: 274, name: "联盟广告" },
+              { value: 235, name: "视频广告" },
+              { value: 400, name: "搜索引擎" }
+            ].sort(function(a, b) {
+              return a.value - b.value;
+            }),
+            roseType: "radius",
+            // 设置周边字体背景色
+            label: {
+              normal: {
+                textStyle: {
+                  color: "rgba(255, 255, 255, 0.3)"
+                }
+              }
+            },
+            // 标识线样式
+            labelLine: {
+              normal: {
+                lineStyle: {
+                  color: "rgba(255, 255, 255, 0.3)"
+                },
+                smooth: 0.2,
+                length: 10,
+                length2: 20
+              }
+            },
+            // 图形配置
+            itemStyle: {
+              normal: {
+                //   图形颜色
+                color: "#c23531",
+                // 图形间阴影效果
+                shadowBlur: 200,
+                shadowColor: "rgba(0, 0, 0, 0.5)"
+              }
+            },
+
+            // 动画效果
+            animationType: "scale",
+            animationEasing: "elasticOut",
+            animationDelay: function(idx) {
+              return Math.random() * 200;
+            }
+          }
+        ]
+      });
+    }
+  }
+};
+</script>
+<style scoped>
+.demo {
+  width: 600px;
+  height: 400px;
+}
+</style>
