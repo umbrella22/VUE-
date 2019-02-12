c6a94d7aa72bce3aaa22a8d1897449455597592a
Sky <1035765115@qq.com>
2019-02-12 22:14:17 +0800

    添加echarts组件，明天完善

---
+++ src/page/echarts/index.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <line></line>
+</div>
+</template>
+<script>
+import line from '@/components/echarts'
+export default {
+    components:line,
+    data:()=>({
+
+    }),
+    methods:{
+        
+    }
+};
+</script>
+<style scoped>
+
+</style>
